5fd22ed1a669fba121c6b2ceacc61a56ece59cba
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-12-28 22:39:37 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const login = userData =>
+  Promise.resolve({
+    user: { name: 'Test User', email: 'test.user@mail.com' },
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmVhMDY0Y2I4ZmY0ODAwMTdhNWRiMjYiLCJpYXQiOjE2MDkxNzU4Nzh9.ufolexUx_gmDMl7-xnAcN5QReRmOHeJnfHjgGfUzFi0',
+  });
+
+export const logout = () => Promise.resolve();
+
+export const getContacts = () =>
+  Promise.resolve([
+    {
+      id: 1,
+      name: 'Bobby',
+      number: '12345',
+    },
+  ]);
+
+export const addContact = payload => Promise.resolve(payload);
+let token = '';
+const setToken = loadedToken => {
+  token = loadedToken;
+};
